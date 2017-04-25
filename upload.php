35dd6dc3f8043635178ebc0badd10cbbc134f22c
spirouac <spirouac@Snowcrash.telus>
2017-04-25 16:52:42 -0700

    added basic PDO outline and something?

--- upload.php
+++ upload.php
@@ -2,15 +2,14 @@
 $target_dir = "";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
+$FileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if file is a file
 if(isset($_POST["submit"])) {
     $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        //echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "File is not cool.";
         $uploadOk = 0;
     }
 }
@@ -30,7 +29,7 @@ if (file_exists($target_file)) {
 //   $uploadOk = 0;
 //}
 // Allow certain file formats
-if($imageFileType != "doc") {
+if($FileType != "doc") {
 	//ungracious exit of php script.
     die("Sorry, only doc file types at this point.");
     $uploadOk = 0;
@@ -41,7 +40,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
