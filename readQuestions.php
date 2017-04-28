9bafa5411b4f68256f116f8f9d98de0b3a658c8d
spirouac <spirouac@Snowcrash.telus>
2017-04-28 16:03:38 -0700

    db scripts

--- readQuestions.php
+++ readQuestions.php
@@ -99,9 +99,9 @@
 				//PDO prefer to use this
 				$insertQuestion = json_encode($questionBank);
 				$stmt = $dbcon->prepare("INSERT INTO question (question, chapter, courseid) VALUES (:question, :chapter :courseid)");
-				$stmt->bind_param(':question', $insertQuestion);
-				$stmt->bind_param(':chapter', $insertChapter);
-				$stmt->bind_param(':courseid', $courseid);
+				$stmt->bindParam(':question', $insertQuestion);
+				$stmt->bindParam(':chapter', $insertChapter);
+				$stmt->bindParam(':courseid', $courseid);
 				$stmt->execute();
 			}
 		}
