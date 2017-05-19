97141d811dc271ea693787cebc4630e2e458a215
root <root@localhost.localdomain>
2017-05-19 15:16:20 -0700

    basic character setup

--- js/play.js
+++ js/play.js
@@ -1,13 +1,26 @@
 var playState = {
   create: function(){
     game.global.background = game.add.sprite(0, 0, 'sky');
-    game.global.roundScoreText = game.add.text(16, game.height - 24, 'Round Score: 0', game.global.mainFont);
     game.global.questionsAnswered = 0;
+
+    game.global.jinny = game.add.sprite(0,0, 'jinny');
+    game.global.jinny.scale.setTo(.1,.1);
+//todo change chars to index array for loop
+    game.global.chars = [];
+    var sprites = ['beaver','rabbit','cat','beaver']
+    for(var i = 0; i < 4; i++){
+	game.global.chars[i] = {};
+	game.global.chars[i].sprite = game.add.sprite((((game.width/4)*(i+1)-game.width/4)) ,game.height - 110,sprites[i]);
+	game.global.chars[i].sprite.scale.setTo(.3,.3);
+	game.global.chars[i].score = game.add.text((((game.width/4)*(i+1)-game.width/4)+game.global.chars[i].sprite.width),game.height - 50, '0', game.global.mainFont);
+    }
+    
+
     game.global.showQuestion(game.global.questions[game.global.questionsAnswered]);
   },
   update: function(){
     //update score text
-    game.global.roundScoreText.text = 'Round Score: ' + game.global.roundStats[game.global.currentRound].score;
+    game.global.chars[0].score.text = game.global.roundStats[game.global.currentRound].score;
     //update answer text position to keep each one moving with its button
     for (var i = 0; i < game.global.buttons.length; i++) {
       game.global.buttons[i].data.text.x = Math.floor(game.global.buttons[i].x + game.global.buttons[i].width / 2);
